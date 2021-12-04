e114d6660f55c6e5e3d817726f493d3af2d28540
grzegorz-karas <grzegorz.s.karas@gmail.com>
2021-12-04 13:43:28 +0100

    fix and refactor test

--- app/tests/test_back_end/test_chi_square.py
+++ app/tests/test_back_end/test_chi_square.py
@@ -6,6 +6,7 @@ import app.back_end.chi_square as chi_square
 
 
 class TestChiSquareTest:
+
     def test_goodness_of_fit(self):
         result = chi_square.test(test_type='goodness-of-fit',
                                  alpha=0.05,
@@ -24,6 +25,32 @@ class TestChiSquareTest:
         }
         assert True
 
+    def test_goodness_of_fit_small(self):
+
+        alpha = 0.05
+
+        result = chi_square.test(
+            test_type='goodness-of-fit',
+            alpha=alpha,
+            n_obs=[[10, 10, 10, 10, 11, 9]],
+            n_exp=[[10, 10, 10, 10, 10, 10]])
+
+        assert result['power'] < 1.2*alpha
+        assert 0.8*alpha < result['power']
+
+    def test_goodness_of_fit_big(self):
+
+        alpha = 0.05
+
+        result = chi_square.test(
+            test_type='goodness-of-fit',
+            alpha=alpha,
+            n_obs=[[100, 100, 100, 100, 101, 99]],
+            n_exp=[[100, 100, 100, 100, 100, 100]])
+
+        assert result['power'] < 1.02*alpha
+        assert 0.98*alpha < result['power']
+
     def test_independence(self):
         res_obs = chi_square.test(test_type='independence', alpha=0.05,
                                   n_obs=[[20, 80], [80, 20]])
@@ -40,6 +67,32 @@ class TestChiSquareTest:
 
         cnft.assert_equal(res_exp, res_obs)
 
+    def test_independence_small(self):
+
+        alpha = 0.05
+
+        result = chi_square.test(
+            test_type='independence',
+            alpha=alpha,
+            n_obs=[[10, 10, 10, 10, 11, 9],
+                   [10, 10, 10, 10, 10, 10]])
+
+        assert result['power'] < 1.2*alpha
+        assert 0.8*alpha < result['power']
+
+    def test_independence_big(self):
+
+        alpha = 0.05
+
+        result = chi_square.test(
+            test_type='independence',
+            alpha=alpha,
+            n_obs=[[100, 100, 100, 100, 101, 99],
+                   [100, 100, 100, 100, 100, 100]])
+
+        assert result['power'] < 1.02*alpha
+        assert 0.98*alpha < result['power']
+
     def test_homogenity(self):
         res_obs = chi_square.test(test_type='homogenity', alpha=0.05,
                                   n_obs=[[20, 80], [80, 20]])
@@ -55,6 +108,32 @@ class TestChiSquareTest:
         }
 
         cnft.assert_equal(res_exp, res_obs)
+
+    def test_homogenity_small(self):
+
+        alpha = 0.05
+
+        result = chi_square.test(
+            test_type='homogenity',
+            alpha=alpha,
+            n_obs=[[10, 10, 10, 10, 11, 9],
+                   [10, 10, 10, 10, 10, 10]])
+
+        assert result['power'] < 1.2*alpha
+        assert 0.8*alpha < result['power']
+
+    def test_homogenity_big(self):
+
+        alpha = 0.05
+
+        result = chi_square.test(
+            test_type='homogenity',
+            alpha=alpha,
+            n_obs=[[100, 100, 100, 100, 101, 99],
+                   [100, 100, 100, 100, 100, 100]])
+
+        assert result['power'] < 1.02*alpha
+        assert 0.98*alpha < result['power']
 
 
 class TestFindNoncentralParam:
@@ -75,6 +154,32 @@ class TestFindNoncentralParam:
 
 class TestSampleSize:
 
+    def test_sample_size_goodness_of_fit_power_equal_target(self):
+
+        res_size = chi_square.test_and_sample_size(
+            test_type='goodness-of-fit',
+            alpha=0.05,
+            target_power=0.8,
+            n_obs=[[round(10/60*60), round(10/60*60), round(10/60*60),
+                    round(10/60*60), round(11/60*60), round(9/60*60)]],
+            n_exp=[[round(10/60*60), round(10/60*60), round(10/60*60),
+                    round(10/60*60), round(10/60*60), round(10/60*60)]])
+
+        n = res_size['target_power_sample_size']
+
+        res_test = chi_square.test(
+            test_type='goodness-of-fit',
+            alpha=0.05,
+            n_obs=[[round(10/60*n), round(10/60*n), round(10/60*n),
+                    round(10/60*n), round(11/60*n), round(9/60*n)]],
+            n_exp=[[round(10/60*n), round(10/60*n), round(10/60*n),
+                    round(10/60*n), round(10/60*n), round(10/60*n)]])
+
+        destination_power = res_size['target_power']
+        obtained_power = res_test['power']
+
+        assert np.isclose(obtained_power, destination_power, rtol=0.01)
+
     def test_sample_size_goodness_of_fit_perfect_match(self):
 
         alpha = 0.05
@@ -88,55 +193,27 @@ class TestSampleSize:
 
         assert result['target_power_sample_size'] == None
 
-    def test_sample_size_goodness_of_fit(self):
-
-        alpha = 0.05
-
-        result = chi_square.test_and_sample_size(
-            test_type='goodness-of-fit',
-            alpha=alpha,
-            target_power=0.8,
-            n_obs=[[10, 10, 10, 10, 11, 9]],
-            n_exp=[[10, 10, 10, 10, 10, 10]])
-
-        assert result['power'] < 2*alpha
-
-    def test_sample_size_goodness_of_fit_power_equal_target(self):
-
-        result = chi_square.test_and_sample_size(
-            test_type='goodness-of-fit',
-            alpha=0.05,
-            target_power=0.8,
-            n_obs=[[round(10/60*3870), round(10/60*3870), round(10/60*3870),
-                    round(10/60*3870), round(11/60*3870), round(9/60*3870)]],
-            n_exp=[[round(10/60*3870), round(10/60*3870), round(10/60*3870),
-                    round(10/60*3870), round(10/60*3870), round(10/60*3870)]])
-
-        assert result['target_power'] < result['power']
-
-    def test_sample_size_independence(self):
-
-        alpha = 0.05
-
-        result = chi_square.test_and_sample_size(
-            test_type='independence',
-            alpha=alpha,
-            target_power=0.8,
-            n_obs=[[10, 10, 10, 10, 11, 9],
-                   [10, 10, 10, 10, 10, 10]])
-
-        assert result['power'] < 2*alpha
-
     def test_sample_size_independence_power_equal_target(self):
 
-        result = chi_square.test_and_sample_size(
+        res_size = chi_square.test_and_sample_size(
             test_type='independence',
             alpha=0.05,
             target_power=0.8,
-            n_obs=[[round(10/60*5748), round(11/60*5748), round(9/60*5748)],
-                   [round(10/60*5748), round(10/60*5748), round(10/60*5748)]])
+            n_obs=[[round(10/60*60), round(11/60*60), round(9/60*60)],
+                   [round(10/60*60), round(10/60*60), round(10/60*60)]])
 
-        assert result['target_power'] < result['power']
+        n = res_size['target_power_sample_size']
+
+        res_test = chi_square.test(
+            test_type='independence',
+            alpha=0.05,
+            n_obs=[[round(10/60*n), round(11/60*n), round(9/60*n)],
+                   [round(10/60*n), round(10/60*n), round(10/60*n)]])
+
+        destination_power = res_size['target_power']
+        obtained_power = res_test['power']
+
+        assert np.isclose(obtained_power, destination_power, rtol=0.01)
 
     def test_sample_size_independence_perfect_match(self):
 
@@ -144,6 +221,41 @@ class TestSampleSize:
 
         result = chi_square.test_and_sample_size(
             test_type='independence',
+            alpha=alpha,
+            target_power=0.8,
+            n_obs=[[10, 10, 10, 10, 10, 10],
+                   [10, 10, 10, 10, 10, 10]])
+
+        assert result['target_power_sample_size'] == None
+
+    def test_sample_size_homogenity_power_equal_target(self):
+
+        res_size = chi_square.test_and_sample_size(
+            test_type='homogenity',
+            alpha=0.05,
+            target_power=0.8,
+            n_obs=[[round(10/60*60), round(11/60*60), round(9/60*60)],
+                   [round(10/60*60), round(10/60*60), round(10/60*60)]])
+
+        n = res_size['target_power_sample_size']
+
+        res_test = chi_square.test(
+            test_type='homogenity',
+            alpha=0.05,
+            n_obs=[[round(10/60*n), round(11/60*n), round(9/60*n)],
+                   [round(10/60*n), round(10/60*n), round(10/60*n)]])
+
+        destination_power = res_size['target_power']
+        obtained_power = res_test['power']
+
+        assert np.isclose(obtained_power, destination_power, rtol=0.01)
+
+    def test_sample_size_homogenity_perfect_match(self):
+
+        alpha = 0.05
+
+        result = chi_square.test_and_sample_size(
+            test_type='homogenity',
             alpha=alpha,
             target_power=0.8,
             n_obs=[[10, 10, 10, 10, 10, 10],
